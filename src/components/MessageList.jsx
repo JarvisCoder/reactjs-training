b35b46b668af8e486e3eabc0f6fda6cf7d1d9b08
Arjun Pandiyan <pandiyan.arjun@gmail.com>
2017-01-07 20:16:33 -0800

    read from firebase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,24 +3,51 @@ import Message from './Message.jsx';
 
 import Card from 'material-ui/Card';
 import List from 'material-ui/List';
+import * as firebase from 'firebase';
+import _ from 'lodash';
 
 class MessageList extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			messages: [
-				'Chat1: Hi there! How are you ?',
-				'Chat2: I am fine, and you ?',
-				'Chat3: Awesome ;)'
-			]
+			messages: []
 		};
+	}
+	componentDidMount() {
+		const config = {
+			apiKey: "AIzaSyBBx-AbX7GDz2M6ulQ51epv-NGFQVhRyqU",
+			authDomain: "lifetripz-services.firebaseapp.com",
+			databaseURL: "https://lifetripz-services.firebaseio.com",
+			storageBucket: "lifetripz-services.appspot.com",
+			messagingSenderId: "616745753888"
+		};
+		firebase.initializeApp(config);
+		const rootRef = firebase.database().ref();
+		const messagesRef = rootRef.child('messages');
+		messagesRef.once('value')
+			.then((snapshot) => {
+				var messagesVal = snapshot.val();
+				var messages = _(messagesVal)
+					.keys()
+					.map((messageKey)=>{
+							var clone = _.clone(messagesVal[messageKey]);
+							clone.key = messageKey;
+							return clone;
+					})
+					.value();
+				this.setState({
+					messages: messages
+				});
+			}
+		);
 	}
 
 	render() {
+		console.log(this.state.messages);
 		var messageNodes = this.state.messages.map((message)=>{
 			return (
-				<Message key={message.length} chatMsg={message}/>
+				<Message key={message.key} chatMsg={message.message}/>
 			);
 		});
 
